653be92c4153c85aa70f5418907b2a0f22424222
valeriaurzua524 <val.urzua05@gmail.com>
2025-04-21 13:07:31 -0700

    updated visitor manager

--- VisitorManager.java
+++ VisitorManager.java
@@ -1,3 +1,37 @@
-public class VisitorManager {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitorManager implements VisitorManagerInterface {
+    private List<Visitor> visitorList;
+
+    public VisitorManager() {
+        visitorList = new ArrayList<>();
+    }
+
+    // add a visitor
+    @Override
+    public void addVisitor(String name, LocalDate visitDate, String reasonForVisiting, String feedback, String gradeLevel) {
+        Visitor newVisitor = new Visitor(name, visitDate, reasonForVisiting, feedback, gradeLevel);
+        visitorList.add(newVisitor);
+    }
+
+    @Override
+    public void displayVisitors() 
+
+
+    @Override
+    public List<Visitor> filterVisitorsByDate(LocalDate date) 
+
+
+    @Override
+    public void displayVisitorsByDate(LocalDate date) 
+
+
+    @Override
+    public void displayVisitorsByGrade(String gradeLevel) 
     
+
+    @Override
+    public void removeVisitor(String name) 
 }
